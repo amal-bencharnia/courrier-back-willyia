9d4e1cde3f3b9ab02bdaa0104c21ba0d4986ba0d
dell <bencharniaamal2@gmail.com>
2021-05-08 12:11:51 +0000

    second commit

---
+++ src/main/java/com/wilaya/courrierbackwillyia/CourrierBackWillyiaApplication.java
@@ -0,0 +1,13 @@
+package com.wilaya.courrierbackwillyia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourrierBackWillyiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourrierBackWillyiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wilaya/courrierbackwillyia/CourrierBackWillyiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilaya.courrierbackwillyia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourrierBackWillyiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
